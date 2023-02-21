6205e07060c37e0ba826f19831f8fefb1b19cc07
Qomar034 <nurqomarudin034@gmail.com>
2023-02-21 22:45:40 +0700

    Create Notification screen and notification card

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home";
+import Notification from "./screens/Notification";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home/>      
+      <Notification/>
+      {/* <Home/>       */}
     </NavigationContainer>
   );
 }
